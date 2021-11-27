60644596fbc3e0f97df18b70413c6c500646bd12
Renan Moura <naneraruom@gmail.com>
2021-11-27 10:06:45 -0300

    1001 javascript

--- 1001/1001.js
+++ 1001/1001.js
@@ -1,6 +1,5 @@
-let input = require("fs").readFileSync("stdin", "utf-8")
+let input = require("fs").readFileSync("1001/stdin", "utf-8")
 let lines = input.split("\n")
-
 
 let A = parseInt(lines.shift())
 let B = parseInt(lines.shift())
